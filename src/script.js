ad66e73ddffb781f54590bf753b7fdb34669521b
Pavel Ecer <ecerpavel@gmail.com>
2022-04-29 23:41:59 +0200

    form attached

--- src/script.js
+++ src/script.js
@@ -40,23 +40,58 @@ document.addEventListener("DOMContentLoaded", function (event) {
   if (gallery) {
     galleryItems = gallery.querySelectorAll(".gallery-item");
     showSlides();
+    // showPhotos(slideIndex);
   }
   if (document.getElementsByClassName("guerilla-slide").length > 0) {
     showGuerillaSlides(guerillaSlideIndex);
   }
 });
 
+function plusPhoto(n) {
+  showPhotos((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentPhoto(n) {
+  showPhotos((slideIndex = n));
+}
+
+function showPhotos(n) {
+  let i;
+  let slides = document.getElementsByClassName("gallery-item");
+  let dots = document.getElementsByClassName("gallery-dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" gallery-active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " gallery-active";
+}
+
 function showSlides() {
   let i;
   let slides = gallery.querySelectorAll(".gallery-item");
+  let dots = document.getElementsByClassName("gallery-dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" gallery-active", "");
   }
   slideIndex++;
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " gallery-active";
   setTimeout(showSlides, 3000);
 }
 
@@ -106,29 +141,51 @@ const form = document.getElementById("myForm");
 
 if (form) {
   form.addEventListener("submit", (event) => {
-    console.log(event);
     event.preventDefault();
     // handle the form data
 
-    const M =
-      "entry.1754747753=prvni&entry.1205926966=&entry.454974113=&entry.604248877=ctvrty&entry.798225549=paty&entry.523846686=sesty";
+    const hen = document.getElementById("hen").checked;
+    const field = document.getElementById("field").checked;
+    const patch = document.getElementById("garden").checked;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const animal = document.getElementById("animal").value;
+    const note = document.getElementById("note").value;
 
-    fetch(
-      "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfx6MsGDo9iY4sv0s21ZzDU8GaOs90OnWNw_dVd_wHjoWSc9A/formResponse?" +
-        M,
-      { method: "POST" }
-    )
-      .then(function (e) {
-        return e.json();
-      })
-      .then(function () {
-        return !1;
+    let url;
+
+    if (hen) {
+      const option = "adopce slepice";
+      const baseUrl =
+        "https://docs.google.com/forms/d/1wa6JQ8A5scCZgTKim5aCj-Xo3BQoOBjFtvpeZ-hd5RQ";
+      const params = `/formResponse?entry.1129579714=${option}&entry.440058418=${name}&entry.1940677981=${email}&entry.609218601=${phone}&entry.1837425085=${animal}&entry.385425575=${note}`;
+      url = baseUrl + params;
+    } else if (patch) {
+      const option = "vlastní záhonek";
+      const baseUrl =
+        "https://docs.google.com/forms/d/1ebURV3eJuB9SpIyYRRhzN0fjopaFCLrGXJPLVoE1OZE";
+      const params = `/formResponse?entry.1992948661=${option}&entry.927632810=${name}&entry.445593250=${email}&entry.1237515599=${phone}&entry.1209230437=${note}`;
+      url = baseUrl + params;
+    } else if (field) {
+      const option = "vlastní políčko";
+      const baseUrl =
+        "https://docs.google.com/forms/d/1ebURV3eJuB9SpIyYRRhzN0fjopaFCLrGXJPLVoE1OZE";
+      const params = `/formResponse?entry.1992948661=${option}&entry.927632810=${name}&entry.445593250=${email}&entry.1237515599=${phone}&entry.1209230437=${note}`;
+      url = baseUrl + params;
+    }
+
+    fetch(url, { method: "POST", mode: "no-cors" })
+      .then((response) => {
+        alert("Formulář odeslán | Form sent");
+        console.log(response);
       })
       .catch(function (e) {
         console.error("Error:", e);
-      }),
-      alert(e({ id: "contact-sent-message" })),
-      Object(u.c)("/");
+        alert(
+          "Stala se chyba, zkuste prosím znovu později. | An error occured, please try again later."
+        );
+      });
   });
 }
 
@@ -146,162 +203,3 @@ function toggleLang(langId) {
   const newUrl = location.origin + newPath;
   location.assign(newUrl);
 }
-
-// fetch(
-//   "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfx6MsGDo9iY4sv0s21ZzDU8GaOs90OnWNw_dVd_wHjoWSc9A/formResponse?" +
-//     M,
-//   { method: "POST" }
-// )
-//   .then(function (e) {
-//     return e.json();
-//   })
-//   .then(function () {
-//     return !1;
-//   })
-//   .catch(function (e) {
-//     console.error("Error:", e);
-//   }),
-// alert(e({ id: "contact-sent-message" })),
-// Object(u.c)("/");
-
-// document.addEventListener(
-//   "scroll",
-//   function () {
-//     const messageText = isInViewport(gallery)
-//       ? "The box is visible in the viewport"
-//       : "The box is not visible in the viewport";
-
-//     if (isInViewport(gallery)) {
-//     }
-
-//     console.log(messageText);
-//   },
-//   {
-//     passive: true,
-//   }
-// );
-
-// var getWindowSize = (function () {
-//   var docEl = document.documentElement,
-//     IS_BODY_ACTING_ROOT = docEl && docEl.clientHeight === 0;
-
-//   // Used to feature test Opera returning wrong values
-//   // for documentElement.clientHeight.
-//   function isDocumentElementHeightOff() {
-//     var d = document,
-//       div = d.createElement("div");
-//     div.style.height = "2500px";
-//     d.body.insertBefore(div, d.body.firstChild);
-//     var r = d.documentElement.clientHeight > 2400;
-//     d.body.removeChild(div);
-//     return r;
-//   }
-
-//   if (typeof document.clientWidth == "number") {
-//     return function () {
-//       return { width: document.clientWidth, height: document.clientHeight };
-//     };
-//   } else if (IS_BODY_ACTING_ROOT || isDocumentElementHeightOff()) {
-//     var b = document.body;
-//     return function () {
-//       return { width: b.clientWidth, height: b.clientHeight };
-//     };
-//   } else {
-//     return function () {
-//       return { width: docEl.clientWidth, height: docEl.clientHeight };
-//     };
-//   }
-// })();
-
-// var minLeft, maxLeft;
-// var scrollDownCount = 0;
-
-// function verticalScroll(e) {
-//   e.preventDefault();
-//   if (e.deltaY < 0) {
-//     document.getElementById("section-1").scrollIntoView({ behavior: "smooth" });
-//   }
-// }
-
-// function horizontalScroll(e) {
-//   e.preventDefault();
-//   var deltaY = e.deltaY;
-//   var preview = document.getElementById("preview");
-//   var prevLeftString = window
-//     .getComputedStyle(preview)
-//     .getPropertyValue("left");
-
-//   var prevLeft = prevLeftString.substring(0, prevLeftString.length - 2);
-//   var computedLeft = prevLeft - deltaY / 3;
-
-//   if (computedLeft < minLeft) {
-//     computedLeft = minLeft;
-//     if (scrollDownCount < 5) {
-//       scrollDownCount += 1;
-//     } else {
-//       document
-//         .getElementById("section-2")
-//         .scrollIntoView({ behavior: "smooth" });
-//       scrollDownCount = 0;
-//     }
-//   } else if (computedLeft > maxLeft) {
-//     computedLeft = maxLeft;
-//   }
-//   preview.style.left = computedLeft + "px";
-// }
-
-// document.addEventListener("DOMContentLoaded", function (event) {
-//   console.log("content loaded", event);
-// var preview = document.getElementById("preview");
-// var section1 = document.getElementById("section-1");
-// var previewItems = document.querySelectorAll(".preview-item");
-// var clientWidth = getWindowSize().width;
-
-// var computedWidth = 0;
-// var firstPreviewWidth = 0;
-
-// previewItems.forEach(function (item, index) {
-//   var itemWidthString = window
-//     .getComputedStyle(item)
-//     .getPropertyValue("width");
-//   var itemWidth = itemWidthString.substring(0, itemWidthString.length - 2);
-//   if (index === 0) {
-//     firstPreviewWidth = itemWidth;
-//   }
-//   computedWidth += +itemWidth;
-// });
-
-// var sectionPaddingString = window
-//   .getComputedStyle(section1)
-//   .getPropertyValue("padding-right");
-// var sectionPaddingRight = sectionPaddingString.substring(
-//   0,
-//   sectionPaddingString.length - 2
-// );
-
-// var left = clientWidth - firstPreviewWidth - sectionPaddingRight;
-
-// preview.style.left = left + "px";
-
-// maxLeft = left;
-// minLeft = left - computedWidth - clientWidth / 5;
-
-// console.log("sectionPaddingRight", sectionPaddingRight);
-// console.log("computedWidth", computedWidth);
-// console.log("left", left);
-// console.log("maxLeft", maxLeft);
-// console.log("minLeft", minLeft);
-// });
-
-// function process_touchmove(event) {
-//   console.log(event);
-// }
-
-// document
-//   .getElementById("section-1")
-//   .addEventListener("wheel", horizontalScroll);
-// document.getElementById("section-2").addEventListener("wheel", verticalScroll);
-// document.getElementById("preview").addEventListener("wheel", horizontalScroll);
-// document
-//   .getElementById("section-1-inner")
-//   .addEventListener("touchmove", process_touchmove, false);
